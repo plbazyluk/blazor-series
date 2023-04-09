4170a39aede88ae3d6c4e0af46d774a3c97fbf9c
plbazyluk <plbazyluk@gmail.com>
2023-04-09 12:56:31 +0200

    feature(module): added wrapping foreach example

---
+++ src/BlazorSeries/Modules/WrappingForeach/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorSeries.Modules.WrappingForeach.Models;
+
+public record User(Guid Id, string FirstName, string LastName, DateTime RegistrationUtc)
+{
+    public readonly static List<User> Source = new()
+    {
+        new(Guid.NewGuid(), "Tony", "Stark", DateTime.UtcNow),
+        new(Guid.NewGuid(), "Steve", "Rogers", DateTime.UtcNow.AddDays(-2)),
+        new(Guid.NewGuid(), "T'challa", string.Empty, DateTime.UtcNow.AddDays(-7)),
+        new(Guid.NewGuid(), "Bruce", "Banner", DateTime.UtcNow.AddDays(-2))
+    };
+}
